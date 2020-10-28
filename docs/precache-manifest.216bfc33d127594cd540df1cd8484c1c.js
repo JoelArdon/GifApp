042374fecf5bd102e8439514e94ba8993a5babdf
Joel <jsalasardon@gmail.com>
2020-10-27 21:35:10 -0600

    publicando

---
+++ docs/precache-manifest.216bfc33d127594cd540df1cd8484c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2314a1dd750ae76f570179640e462106",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62986feaefcd7efefae7",
+    "url": "/static/css/main.1d04e835.chunk.css"
+  },
+  {
+    "revision": "e7255020ef95d0b615ea",
+    "url": "/static/js/2.af8590f8.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.af8590f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62986feaefcd7efefae7",
+    "url": "/static/js/main.0ffb30b0.chunk.js"
+  },
+  {
+    "revision": "331f50860785d869aa95",
+    "url": "/static/js/runtime-main.c61e2e72.js"
+  }
+]);
